282bd1bf0d3349bbac1ff7fc200accf90d10216a
Yuki Kitamura <Pro.ktmr@gmail.com>
2025-04-12 21:43:08 +0900

    fix basePath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const IS_DEV = process.env.NODE_ENV === "development";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: IS_DEV ? "" : "/bindec-game",
 };
 
 export default nextConfig;
