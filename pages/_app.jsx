acf4325651203571217eb27752a8beb5effd5174
Yuki Kitamura <Pro.ktmr@gmail.com>
2025-04-12 21:42:20 +0900

    fix _app

---
+++ pages/_app.jsx
@@ -0,0 +1,22 @@
+import App from "next/app";
+import "../styles/globals.css";
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render () {
+    const { Component, pageProps } = this.props
+
+    return (
+        <Component {...pageProps} />
+    )
+  }
+}
